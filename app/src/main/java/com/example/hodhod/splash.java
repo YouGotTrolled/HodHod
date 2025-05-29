a78d2f6afa8adb807a531cebe467a057981e115e
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-05-29 13:19:41 +0330

    ggg

---
+++ app/src/main/java/com/example/hodhod/splash.java
@@ -0,0 +1,44 @@
+package com.example.hodhod;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+
+    private ImageView logo;
+    private TextView logoName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.t), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        logo = findViewById(R.id.logo);
+        logoName = findViewById(R.id.textView2);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        logo.startAnimation(fadeIn);
+        logoName.startAnimation(fadeIn);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(splash.this, MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.slidein, R.anim.slideout);
+            finish();
+        }, 3000);
+    }
+}
